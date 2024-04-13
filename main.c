0d800aedc9502809f8f0aa8d45163e8fb064ec8e
GuiSM6030 <102614840+GuiSM6030@users.noreply.github.com>
2024-04-13 00:51:00 +0000

    Pequena correção na main

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "tarefas.h"
 
 int main(){
@@ -17,26 +18,26 @@ int main(){
         printf("2 - Deletar tarefa\n");
         printf("3 - Listar tarefas\n");
         printf("4 - Listar tarefas por categoria\n");
-        printf("5 - Exportar tarefas para arquivo de texto|n");
+        printf("5 - Exportar tarefas para arquivo de texto\n");
         printf("6 - Salvar tarefas em arquivo binário\n"); 
         printf("7 - Carregar tarefas de arquivo binário\n");
         printf("0 - Sair\n");
         printf("Escolha uma opcao: ");
 
         scanf("%d", &opcao);
+      
         opcao--;
-        if(opcao > 2)
-            printf("Opcao invalida\n");
-        else if(opcao >= 0)
-            fs[opcao](tarefas, &pos);
-        else {
-            printf("Sair...\n");
-            fs[3](tarefas, &pos); 
-            exit(0);
-        }
-      } while(opcao >= 0);
-
-          return 0; 
+      
+      if(opcao > 4 || opcao < 0)
+          printf("Opcao invalida\n");
+      else if(opcao == 0)
+          fs[opcao](tarefas, &pos);
+      else if(opcao > 0)
+          fs[opcao](tarefas, &pos);
+      else {
+          printf("Saindo...\n");
+          fs[3](tarefas, &pos); 
+          exit(0);
       }
     } while(opcao >= 0);
 
